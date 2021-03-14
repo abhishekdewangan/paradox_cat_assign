d2416ed97e4e14964113cc6530e267658d0f23a3
Dewangan <abhishekdewangan@theporter.in>
2021-03-14 11:45:39 +0530

    added todo

--- src/main/kotlin/DecodingProblem.kt
+++ src/main/kotlin/DecodingProblem.kt
@@ -2,6 +2,7 @@ import afsk_decoder.AFSKDecoder
 
 import java.io.File
 
+// todo currently not reading in buffered manner which will cause memory issue will revisit that later
 fun main() {
   val file = File("/Users/abhishek/Documents/experiment/assignments/src/main/kotlin/file_3.wav")
   val messageBytes = AFSKDecoder().decode(file)
