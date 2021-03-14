0caa976d3e57363662e907d381eac68315feb077
Dewangan <abhishekdewangan@theporter.in>
2021-03-14 11:50:19 +0530

    removed unnecessary line

--- src/main/kotlin/afsk_decoder/AFSKDecoder.kt
+++ src/main/kotlin/afsk_decoder/AFSKDecoder.kt
@@ -5,8 +5,7 @@ import java.io.File
 class AFSKDecoder {
 
   fun decode(file: File): List<Int> =
-    file
-      .let { file -> BitDecoder().decode(file) }
+    BitDecoder().decode(file)
       .let { bitStream -> ByteDecoder().decode(bitStream) }
       .let { byteStream -> MessageDecoder().decode(byteStream) }
 }
