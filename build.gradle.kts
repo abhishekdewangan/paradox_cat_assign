c701210b14761b25ffaa43fdc563a9e66ac1d583
Dewangan <abhishekdewangan@theporter.in>
2021-03-14 11:49:24 +0530

    remove unnecessary dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,14 +7,10 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven {
-        url = uri("https://jitpack.io")
-    }
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("com.github.mbr4477:tr-wave:0.2.0")
 }
 
 tasks {
